f891fd21222e294dc0f3ff81f17d6deb512302bd
toniagne <toniagne@gmail.com>
2017-07-11 11:47:19 -0300

    ARQUIVOS INICIO

---
+++ www/js/controllers.js
@@ -0,0 +1,50 @@
+angular.module('starter.controllers', ['ngSanitize'])
+
+.controller('JornalApp', function($scope, $sce, $ionicLoading, $timeout) {
+
+  $ionicLoading.show({
+    content: 'Loading',
+    animation: 'fade-in',
+    showBackdrop: true,
+    maxWidth: 200,
+    showDelay: 0
+  });
+
+  $scope.trustSrc = function(src) {
+    $timeout(function () {
+    $ionicLoading.hide();
+  }, 2000);
+      return $sce.trustAsResourceUrl(src);
+    }
+
+  $scope.iframeURL = "http://www.jornaldopovo.com.br/mobile/site/index.php";
+
+
+})
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
