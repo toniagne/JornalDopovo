8143c921f97767a55ab5db03a599285a88d4dca8
toniagne <toniagne@gmail.com>
2017-07-19 14:23:04 -0300

    ALTERAÇÕES

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,80 @@
-angular.module('starter.controllers', ['ngSanitize', 'ngMask', 'ngPDFViewer'])
+angular.module('starter.controllers', ['ngSanitize', 'ngMask', 'ion-floating-menu'])
 
-.controller('JornalApp', function($scope, $sce, $ionicLoading, $timeout) {
+.controller('JornalApp', function($scope, $sce, $ionicLoading, $timeout, $location, $ionicModal, $http) {
+
+  var url =  'http://www.jornaldopovo.com.br/jpApp/edicoes.php?callback=JSON_CALLBACK';
+   $http.jsonp(url).
+   success(function(data, status, headers, config) {
+      $scope.edicoes = data;
+      console.log(data);
+   }).
+   error(function(data, status, headers, config) {
+     console.log('erro');
+   });
+
+
+
+   $ionicModal.fromTemplateUrl('templates/edicao-selecionada.html', {
+     scope: $scope,
+     controller: 'ChatsCtrl',
+   }).then(function(modal) {
+     $scope.edicaoSelecionada = modal;
+   });
+
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope,
+    controller: 'ChatsCtrl',
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  $ionicModal.fromTemplateUrl('templates/tab-chats.html', {
+    scope: $scope,
+    controller: 'ChatsCtrl',
+  }).then(function(modal) {
+    $scope.edicoesImpressas = modal;
+  });
+
+  $ionicModal.fromTemplateUrl('templates/tab-assinar.html', {
+    scope: $scope,
+    controller: 'ChatsCtrl',
+  }).then(function(modal) {
+    $scope.assinaturasJanela = modal;
+  });
+
+
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+  $scope.closeImpreesa = function() {
+    $scope.edicoesImpressas.hide();
+  };
+  $scope.closeAssinaturas = function() {
+    $scope.assinaturasJanela.hide();
+  };
+  $scope.closeEdicoes = function() {
+    $scope.edicaoSelecionada.hide();
+  };
+
+
+  // Open the login modal
+  $scope.versaoImpressa = function(coditem) {
+    $scope.edicoesImpressas.show();
+  };
+
+  $scope.abreEdicao = function(coditem){
+    console.log(coditem);
+    $scope.modal.show();
+  };
+
+  $scope.assinaturas = function(){
+    $scope.assinaturasJanela.show();
+  };
+
+  $scope.doLogin = function() {
+    $scope.closeLogin();
+    $scope.edicaoSelecionada.show();
+  };
 
   $ionicLoading.show({
     content: 'Loading',
@@ -16,21 +90,26 @@ angular.module('starter.controllers', ['ngSanitize', 'ngMask', 'ngPDFViewer'])
       return $sce.trustAsResourceUrl(src);
     }
   $scope.iframeURL = "http://www.jornaldopovo.com.br/mobile/site/index.php";
-
+  $scope.iframeURLassinatura = "http://www.jornaldopovo.com.br/site/assinar.php";
 
 })
 
-.controller('DashCtrl', function($scope) {})
+
+.controller('DashCtrl', function($scope) {
+})
 
 .controller('LoginCtrl', function($scope) {
-  
+
   $scope.irPg = function(coditem){
     console.log(coditem);
   };
 
 })
 
-.controller('ChatsCtrl', function($scope, $ionicModal, $timeout, $location, $http, PDFViewerService, $sce) {
+.controller('ChatsCtrl', function($scope, $ionicModal, $timeout, $location, $http, $sce) {
+
+
+
 
   var url =  'http://www.jornaldopovo.com.br/jpApp/edicoes.php?callback=JSON_CALLBACK';
    $http.jsonp(url).
@@ -71,7 +150,6 @@ angular.module('starter.controllers', ['ngSanitize', 'ngMask', 'ngPDFViewer'])
  // Open the login modal
  $scope.login = function(coditem) {
    $scope.modal.show();
-   console.log(coditem);
  };
 
  // Perform the login action when the user submits the login form
@@ -83,30 +161,6 @@ angular.module('starter.controllers', ['ngSanitize', 'ngMask', 'ngPDFViewer'])
 
  };
 
- $scope.pdfURL = "img/test.pdf";
-
- 	//$scope.instance = pdf.Instance("viewer");
-
- 	$scope.nextPage = function() {
- 		$scope.instance.nextPage();
- 	};
-
- 	$scope.prevPage = function() {
- 		$scope.instance.prevPage();
- 	};
-
- 	$scope.gotoPage = function(page) {
- 		$scope.instance.gotoPage(page);
- 	};
-
- 	$scope.pageLoaded = function(curPage, totalPages) {
- 		$scope.currentPage = curPage;
- 		$scope.totalPages = totalPages;
- 	};
-
- 	$scope.loadProgress = function(loaded, total, state) {
- 		console.log('loaded =', loaded, 'total =', total, 'state =', state);
- };
 })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
